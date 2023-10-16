56d5f189f2dfa42a078d846f0e6607873ae49eb6
Emil Wennström <emilwennstrom@gmail.com>
2023-10-16 08:22:46 +0200

    Add project files.

---
+++ DistLab2/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DistLab2.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

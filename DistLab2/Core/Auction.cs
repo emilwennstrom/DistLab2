5d198b9e56d23c35fce73cfa2c6f8b674db16577
AlTheMan <algot98@hotmail.com>
2023-10-16 12:36:18 +0200

    action, bid added

--- DistLab2/Core/Auction.cs
+++ DistLab2/Core/Auction.cs
@@ -2,5 +2,12 @@
 {
     public class Auction
     {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        //lägg till user
+        public int StartingPrice { get; set; }
+        public DateOnly CreationDate { get; set; }
+        public DateOnly endDate { get; set; }
+
     }
 }

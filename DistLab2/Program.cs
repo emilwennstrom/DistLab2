d726bac5ed542bdb82cf5ea66ea883ca5a46876e
AlTheMan <algot98@hotmail.com>
2023-10-19 18:05:45 +0200

    Identity added

--- DistLab2/Program.cs
+++ DistLab2/Program.cs
@@ -5,6 +5,9 @@ using DistLab2.Persistence.Interfaces;
 using DistLab2.Persistence.MockClasses;
 using DistLab2.Persistence.Repositories;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using DistLab2.Data;
+using DistLab2.Areas.Identity.Data;
 
 namespace DistLab2
 {
@@ -18,6 +21,11 @@ namespace DistLab2
             builder.Services.AddControllersWithViews();
             
             builder.Services.AddDbContext<AuctionDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("AuctionDbConnection")));
+            builder.Services.AddDbContext<IdentityContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("IdentityDbConnection")));
+
+
+            builder.Services.AddDefaultIdentity<DistUser>(options => options.SignIn.RequireConfirmedAccount = false)
+                .AddEntityFrameworkStores<IdentityContext>();
 
             builder.Services.AddScoped<IAuctionRepository, AuctionRepository>();
 
@@ -44,13 +52,14 @@ namespace DistLab2
             app.UseStaticFiles();
 
             app.UseRouting();
+                        app.UseAuthentication();;
 
             app.UseAuthorization();
 
             app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
-            //app.MapRazorPages();
+            app.MapRazorPages();
             app.Run();
         }
     }
